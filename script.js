5a148a59b0271abe04144907b99d29d0003056c8
Subhajit Roy <roysubhajit8697@gmail.com>
2022-10-05 20:32:31 +0530

    modified & improved navigation for all kinds of screens

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-const navBtn = document.getElementById('nav-btn');
-const cancelBtn = document.getElementById('cancel-btn');
-const sideNav = document.getElementById('sidenav');
-const modal = document.getElementById('modal');
+const navBtn = document.getElementById("nav-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+const sideNav = document.getElementById("sidenav");
+const modal = document.getElementById("modal");
+const scrollBg = document.querySelector(".scroll-bg");
+const navigations = document.querySelectorAll(".navbar li");
 
-navBtn.addEventListener("click", function(){
-    sideNav.classList.add('show');
-    modal.classList.add('showModal');
+navBtn.addEventListener("click", function () {
+  sideNav.classList.add("show");
+  modal.classList.add("showModal");
 });
 
-cancelBtn.addEventListener('click', function(){
-    sideNav.classList.remove('show');
-    modal.classList.remove('showModal');
+cancelBtn.addEventListener("click", function () {
+  sideNav.classList.remove("show");
+  modal.classList.remove("showModal");
 });
 
-window.addEventListener('click', function(event){
-    if(event.target === modal){
-        sideNav.classList.remove('show');
-        modal.classList.remove('showModal');
-    }
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    sideNav.classList.remove("show");
+    modal.classList.remove("showModal");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) scrollBg.classList.add("active");
+  else scrollBg.classList.remove("active");
+});
+
+navigations.forEach((nav) => {
+  nav.addEventListener("click", () => {
+    sideNav.classList.remove("show");
+    modal.classList.remove("showModal");
+  });
 });
